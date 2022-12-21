39a078ae6fdf6ac2f44922bb2a7441e6b4552499
YOONHaeJU <102014853+YoonHaeju@users.noreply.github.com>
2022-12-21 21:44:18 +0900

    최종

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 
-// 오실레이터
+//  오실레이터
 // 0=도 //1=도# // 2=레 // 3=레# // 4=미 // 5=파 // 6=파# // 7=솔 // 8=솔# // 9=라 // 10=라# // 11=시
-var wave = [12];
+var wave = [13];
 
 // 버튼
-var button = [12];
+var button = [13];
 var waveform_button = [4];
 
 var volume_size = 0;
@@ -21,23 +21,25 @@ let waveDiv;
 var volText;
 
 function setup() {  // 화면 세팅공간 (실행시 처음에 한번 실행)
-    createCanvas(displayWidth, displayHeight * 0.9);
-    //createCanvas(500, 900);
+    createCanvas(displayWidth, displayHeight * 0.8);
+    //createCanvas(900, 500);
     background(200, 200, 200);
 
     pianoDiv = createDiv('',
-        button[0] = createButton('C'),
-        button[1] = createButton('C#'),
-        button[2] = createButton('D'),
-        button[3] = createButton('D#'),
-        button[4] = createButton('E'),
-        button[5] = createButton('F'),
-        button[6] = createButton('F#'),
-        button[7] = createButton('G'),
-        button[8] = createButton('G#'),
-        button[9] = createButton('A'),
-        button[10] = createButton('A#'),
-        button[11] = createButton('B')
+        button[0] = createButton(''),
+        button[1] = createButton(''),
+        button[2] = createButton(''),
+        button[3] = createButton(''),
+        button[4] = createButton(''),
+        button[5] = createButton(''),
+        button[6] = createButton(''),
+        button[7] = createButton(''),
+        button[8] = createButton(''),
+        button[9] = createButton(''),
+        button[10] = createButton(''),
+        button[11] = createButton(''),
+        button[12] = createButton('')
+        
     );
     Piano_style();
 
@@ -56,7 +58,7 @@ function setup() {  // 화면 세팅공간 (실행시 처음에 한번 실행)
     }
 
 
-    for (var i = 0; i < 12; i++) {
+    for (var i = 0; i < 13; i++) {
         wave[i] = new p5.Oscillator();
         wave[i].setType(waveform);
         wave[i].amp(volume_size);  // 일단 볼륨 다 0
@@ -74,8 +76,9 @@ function setup() {  // 화면 세팅공간 (실행시 처음에 한번 실행)
     wave[9].freq(440);
     wave[10].freq(466.1638);
     wave[11].freq(493.8833);
+    wave[12].freq(523.2511);
 
-    for (var j = 0; j < 12; j++) {
+    for (var j = 0; j < 13; j++) {
         button[j].touchStarted(eval("Play" + str(j)));
         button[j].touchEnded(eval("Stop" + str(j)));
     }
@@ -88,7 +91,7 @@ function draw() {   // 1초에 60프레임씩 무한 반복
     background(0, 0, 0);
 
     // 사운드
-    for (var k = 0; k < 12; k++) {
+    for (var k = 0; k < 13; k++) {
         wave[k].setType(waveform);
         wave[k].amp(volume_size);  // 일단 볼륨 다 0
     }
@@ -152,6 +155,9 @@ function Stop10() {
 function Stop11() {
     wave[11].stop();
 }
+function Stop12() {
+    wave[12].stop();
+}
 
 
 function Play0() {
@@ -190,6 +196,9 @@ function Play10() {
 function Play11() {
     wave[11].start();
 }
+function Play12() {
+    wave[12].start();
+}
 
 
 
@@ -220,26 +229,26 @@ function Piano_style() {
     let button_col2 = color(100, 100, 100, 2555);
 
     var count1 = 0;
-    for (var j = 11; j >= 0; j--) {
-        if (j == 0 || j == 2 || j == 4 || j == 5 || j == 7 || j == 9 || j == 11) {
-            button[j].position(pianoDiv.width / 10 * 3, pianoDiv.height / 7 * count1);
-            button[j].size(pianoDiv.width / 10 * 7, pianoDiv.height / 7);
+    for (var j = 0; j < 13; j++) {
+        if (j == 0 || j == 2 || j == 4 || j == 5 || j == 7 || j == 9 || j == 11 || j == 12) {
+            button[j].position(pianoDiv.width / 8 * count1, 0);
+            button[j].size(pianoDiv.width / 8, pianoDiv.height);
             button[j].style('font-size', '30px');
-            button[j].style('text-align', 'right');
+            button[j].style('text-align', 'center');
             button[j].style('background-color', button_col1);
             count1++;
             button[j].parent(pianoDiv)
         }
     }
     var count2 = 0;
-    for (var i = 11; i >= 0; i--) {
+    for (var i = 0; i <13; i++) {
         if (i == 1 || i == 3 || i == 6 || i == 8 || i == 10) {
-            button[i].position(0, pianoDiv.height / 7 * (count2) + pianoDiv.height / 14);
-            button[i].size(pianoDiv.width / 5 * 3, pianoDiv.height / 7);
+            button[i].position(pianoDiv.width / 8 * (count2) + pianoDiv.width / 16, 0);
+            button[i].size(pianoDiv.width / 8, pianoDiv.height / 5 * 3);
             button[i].style('font-size', '30px');
-            button[i].style('text-align', 'right');
+            button[i].style('text-align', 'center');
             button[i].style('background-color', button_col2);
-            if (i == 6) count2++;
+            if (i == 3) count2++;
             count2++;
             button[i].parent(pianoDiv);
         }
@@ -277,14 +286,14 @@ function waveDiv_style() {
 
 function deviceMoved() {   // 디바이스 앞뒤로 꺽으면 색, 볼륨 변경
     //background(rotationX * 2, 200, 200);
-    text(rotationX, 50, 50);
+    //text(rotationY, 50, 50);
 
     // 일어나서 연주하는 것을 기본으로
-    if (rotationX > -30 && rotationX <= 60) {
-        volume_size = (rotationX + 30) / 90;
-    } else if (rotationX > 60 && rotationX <= 180) {
+    if (rotationY > -30 && rotationY <= 60) {
+        volume_size = (rotationY + 30) / 90;
+    } else if (rotationY > 60 && rotationY <= 180) {
         volume_size = 1;
-    } else if (rotationX <= -30 && rotationX > -180) {
+    } else if (rotationY <= -30 && rotationY > -180) {
         volume_size = 0;
     }
 }
